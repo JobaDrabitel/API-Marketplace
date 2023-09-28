c39d5bcd60c3d236616405ebf1a0a67bf50418a6
legen <legenadary.pigeon@gmail.com>
2023-09-28 14:51:11 +0300

    fix

---
+++ API Handlers/AutoMapper.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Linq.Dynamic.Core;
+using System.Text.Json;
+
+namespace API_Marketplace_.net_7_v1.API_Handlers
+{
+	public class MappingProfile<T, TCollection> : Profile
+	where T : class
+	where TCollection : class
+	{
+		public MappingProfile()
+		{
+			CreateMap<T, TCollection>();
+		}
+	}
+
+}
